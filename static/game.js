7b6add3e9941e6ed171d2ee81a60d6f192fae62f
Aidan <aidanwhunt@gmail.com>
2020-07-19 20:51:39 +1000

    Made iteration of mp from tutorial

--- static/game.js
+++ static/game.js
@@ -1,29 +1,61 @@
-import {getRenderer} from './renderer.js'
+let socket = io();
 
-class Character {
-    constructor() {
-        this._x = 95;
-        this._y = 40;
-    }
-
-    draw() {
-        let r = getRenderer();
-        let ctx = r.getContext();
-        console.log(ctx);
-
-        ctx.beginPath();
-        ctx.arc(this._x, this._y, 20, 0, 2 * Math.PI);
-        ctx.stroke();
-    }
-
-    move(mx, my) {
-        this._x += mx;
-        this._y += my;
-    }
+let movement = {
+  up: false,
+  down: false,
+  left: false,
+  right: false
 }
+document.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      movement.left = true;
+      break;
+    case 87: // W
+      movement.up = true;
+      break;
+    case 68: // D
+      movement.right = true;
+      break;
+    case 83: // S
+      movement.down = true;
+      break;
+  }
+});
+document.addEventListener('keyup', function(event) {
+  switch (event.keyCode) {
+    case 65: // A
+      movement.left = false;
+      break;
+    case 87: // W
+      movement.up = false;
+      break;
+    case 68: // D
+      movement.right = false;
+      break;
+    case 83: // S
+      movement.down = false;
+      break;
+  }
+});
 
-(function launch() {
-    let char = new Character();
-    char.draw();
-    console.log("test");
-})()
+socket.emit('new player');
+setInterval(function() {
+  socket.emit('movement', movement);
+}, 1000 / 60);
+
+let canvas = document.getElementById('canvas');
+canvas.width = 800;
+canvas.height = 600;
+let context = canvas.getContext('2d');
+socket.on('state', function(players) {
+	console.log("New state received")
+  context.clearRect(0, 0, 800, 600);
+  context.fillStyle = 'green';
+  for (let id in players) {
+    let player = players[id];
+    context.beginPath();
+    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    context.fill();
+  }
+});
