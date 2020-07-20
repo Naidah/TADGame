4b94e56b0c8eebd33f9e140b2e7ebf4410f3343b
Naidah <aidanwhunt@gmail.com>
2020-07-20 16:32:02 +1000

    Updated file indentation

--- static/game.js
+++ static/game.js
@@ -1,61 +1,61 @@
 let socket = io();
 
 let movement = {
-  up: false,
-  down: false,
-  left: false,
-  right: false
+    up: false,
+    down: false,
+    left: false,
+    right: false
 }
 document.addEventListener('keydown', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = true;
-      break;
-    case 87: // W
-      movement.up = true;
-      break;
-    case 68: // D
-      movement.right = true;
-      break;
-    case 83: // S
-      movement.down = true;
-      break;
-  }
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = true;
+            break;
+        case 87: // W
+            movement.up = true;
+            break;
+        case 68: // D
+            movement.right = true;
+            break;
+        case 83: // S
+            movement.down = true;
+            break;
+    }
 });
 document.addEventListener('keyup', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = false;
-      break;
-    case 87: // W
-      movement.up = false;
-      break;
-    case 68: // D
-      movement.right = false;
-      break;
-    case 83: // S
-      movement.down = false;
-      break;
-  }
+    switch (event.keyCode) {
+        case 65: // A
+            movement.left = false;
+            break;
+        case 87: // W
+            movement.up = false;
+            break;
+        case 68: // D
+            movement.right = false;
+            break;
+        case 83: // S
+            movement.down = false;
+            break;
+    }
 });
 
 socket.emit('new player');
 setInterval(function() {
-  socket.emit('movement', movement);
+    socket.emit('movement', movement);
 }, 1000 / 60);
 
 let canvas = document.getElementById('canvas');
 canvas.width = 800;
 canvas.height = 600;
 let context = canvas.getContext('2d');
+
 socket.on('state', function(players) {
-	console.log("New state received")
-  context.clearRect(0, 0, 800, 600);
-  context.fillStyle = 'green';
-  for (let id in players) {
-    let player = players[id];
-    context.beginPath();
-    context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-    context.fill();
-  }
+    context.clearRect(0, 0, 800, 600);
+    context.fillStyle = 'green';
+    for (let id in players) {
+        let player = players[id];
+        context.beginPath();
+        context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+        context.fill();
+    }
 });
