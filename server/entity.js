e08789d0c035f3f34b2f7e0ce13c209c518307dc
Naidah <aidanwhunt@gmail.com>
2020-07-22 19:47:23 +1000

    Updated build proceedure to allow for typescript

--- server/entity.js
+++ server/entity.js
@@ -1,23 +1,23 @@
 export class Entity {
-    constructor(x, y) {
-        this._x = x;
-        this._y = y;
-        this._direction = 0;
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+    this._direction = 0;
 
-        this._sx = 0;
-        this._sy = 0;
-    }
+    this._sx = 0;
+    this._sy = 0;
+  }
 
-    update(delta) {
-        this._x += this._sx * delta;
-        this._y += this._sy * delta;
-    }
+  update(delta) {
+    this._x += this._sx * delta;
+    this._y += this._sy * delta;
+  }
 
-    getRepr() {
-        return {
-            x: this._x,
-            y: this._y,
-            direction: this._direction
-        }
-    }
+  getRepr() {
+    return {
+      x: this._x,
+      y: this._y,
+      direction: this._direction,
+    };
+  }
 }
